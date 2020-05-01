20ce138a218b8f7591a92ec6d34cd4d73ebb94db
Lucas Brito <lucashbrito@outlook.com>
2020-05-01 16:09:10 -0300

    IntroduceLocalExtesion and IntroducerForeingMethod

--- RefactoringApp/MovingFeaturesBetweenObjects/IntroduceLocalExtesion/UsingWrapper.cs
+++ RefactoringApp/MovingFeaturesBetweenObjects/IntroduceLocalExtesion/UsingWrapper.cs
@@ -10,7 +10,7 @@ namespace RefactoringApp.MovingFeaturesBetweenObjects.IntroduceLocalExtesion
             var newStart = new DateTime(2020, 01, 01 + 1);
         }
 
-        void ExampleUsingWrapper()
+        void ExampleUsingWrapper() 
         {
             var nextStart = PreviousDate.NextStart;
         }
