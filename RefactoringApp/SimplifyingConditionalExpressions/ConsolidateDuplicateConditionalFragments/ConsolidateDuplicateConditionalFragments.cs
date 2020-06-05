e4496c484534964454ab646a932f65c16ae0ef6b
Jhemys Alves <jhemys.alves@sovos.com>
2020-06-05 09:56:07 -0300

    ConsolidateDuplicateConditionalFragments and RemoveControlFlag

---
+++ RefactoringApp/SimplifyingConditionalExpressions/ConsolidateDuplicateConditionalFragments/ConsolidateDuplicateConditionalFragments.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RefactoringApp.SimplifyingConditionalExpressions.ConsolidateDuplicateConditionalFragments
+{
+    public class ConsolidateDuplicateConditionalFragments
+    {
+        public void DuplicateConditional()
+        {
+            var value = new Random().Next(0, 10);
+            if (Validation(value))
+            {
+                DoThis();
+                Console.WriteLine(value * 100);
+                DoThat();
+            }
+            else
+            {
+                DoThis();
+                Console.WriteLine(value * 12);
+                DoThat();
+            }
+        }
+
+        public void DuplicateConditionalRefactored()
+        {
+            var value = new Random().Next(0, 10);
+            
+            DoThis();
+            
+            if (Validation(value))
+            {
+                Console.WriteLine(value * 100);
+            }
+            else
+            {
+                Console.WriteLine(value * 12);
+            }
+
+            DoThat();
+        }
+
+        public bool Validation(int value)
+        {
+            return value >= 5;
+        }
+
+        public void DoThis()
+        {
+
+        }
+
+        public void DoThat()
+        {
+
+        }
+    }
+}
---
+++ RefactoringApp/SimplifyingConditionalExpressions/RemoveControlFlag/RemoveControlFlag.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefactoringApp.SimplifyingConditionalExpressions.RemoveControlFlag
+{
+    class RemoveControlFlag
+    {
+        public void ControlFlag()
+        {
+            var value = new Random().Next(0, 250);
+            var found = false;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (value == i)
+                {
+                    found = true;
+                }
+            }
+
+            if (found)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        public void ControlFlagRefactored()
+        {
+            var value = new Random().Next(0, 250);
+            for (int i = 0; i <= 100; i++)
+            {
+                if (value == i)
+                {
+                    Console.WriteLine(value);
+                    break;
+                }
+            }
+        }
+    }
+}
